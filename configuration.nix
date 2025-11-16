542ca409e134d7a6ef25c290a6aa8374eb916635
Josh Zeller <jdzeller18@gmail.com>
2025-11-16 00:45:07 -0600

    Copy current configuration.nix from /etc/nixos

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,13 @@
 
   programs.firefox.enable = true;
   programs.git.enable = true;
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "jzeller" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
