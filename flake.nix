efe2f4139f5358eb0691b1a6e18debacf6fa63ec
Josh Zeller <jdzeller18@gmail.com>
2025-12-25 00:28:01 -0600

    Add new host and refactor for flake compatability

--- flake.nix
+++ flake.nix
@@ -9,9 +9,19 @@
     nixosConfigurations.shadow = nixpkgs.lib.nixosSystem {
       modules = [
         ./hosts/shadow/configuration.nix
+        ./modules/common.nix
+        ./modules/desktop.nix
         ./modules/nvidia.nix
         ./modules/users.nix
         ./modules/plasma6.nix
+      ];
+    };
+    nixosConfigurations.meerkat = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./hosts/meerkat/configuration.nix
+        ./modules/common.nix
+        ./modules/server.nix
+        ./modules/users.nix
       ];
     };
   };
