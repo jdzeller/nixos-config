893aa410e71576a190733a84504cd2c872dfefa6
Josh Zeller <jdzeller18@gmail.com>
2025-12-25 00:28:30 -0600

    New modules definitions

---
+++ modules/common.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Don't show bootloader countdown
+  boot.loader.timeout = 0;
+
+  # Enable Plymouth at boot
+  boot.plymouth.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Common programs and packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.git.enable = true;
+  programs.neovim.enable = true;
+  programs.htop.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    fastfetch
+  ];
+}
---
+++ modules/desktop.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Enable networkmanager openconnect 
+  networking.networkmanager.plugins = with pkgs; [ networkmanager-openconnect ];
+
+  # Enable flatpak
+  services.flatpak.enable = true;
+
+  # Desktop Applications and packages 
+  programs.firefox.enable = true;
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "jzeller" ];
+  };
+  programs.vscode.enable = true;
+  programs.partition-manager.enable = true;
+  programs.steam.enable = true;  
+  programs.kdeconnect.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    papirus-icon-theme
+  ];
+}
