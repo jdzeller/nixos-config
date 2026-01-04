92b1e0146f7d9f1979de5235411c3953c82029ff
Josh Zeller <jdzeller18@gmail.com>
2026-01-04 15:25:10 -0600

    Add Cosmic desktop

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
         ./modules/nvidia.nix
         ./modules/users.nix
         ./modules/plasma6.nix
+	./modules/cosmic.nix
       ];
     };
     nixosConfigurations.meerkat = nixpkgs.lib.nixosSystem {
