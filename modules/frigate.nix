770ca62a921c4ba933625610c7c34f411bac9398
Josh Zeller <jdzeller18@gmail.com>
2026-01-12 23:35:47 -0600

    Add openvino

--- modules/frigate.nix
+++ modules/frigate.nix
@@ -4,6 +4,9 @@
   fileSystems."/media/frigate" = {
     device = "192.168.1.8:/mnt/user/frigate";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "rw" ];
   };
+
+  environment.systemPackages = [
+    pkgs.openvino
+  ];
 }
